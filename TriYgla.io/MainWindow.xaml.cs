b792e0e39c2503c77f8f8d9a013627effcc73a8e
jovev <jovev@DESKTOP-PB89KQ8>
2024-10-22 09:58:56 +0300

    Добавьте файлы проекта.

---
+++ TriYgla.io/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+
+namespace TriangleApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void E_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(SideA.Text);
+                double b = double.Parse(SideB.Text);
+                double c = double.Parse(SideC.Text);
+
+                string result;
+
+                if (a + b <= c || a + c <= b || b + c <= a)
+                {
+                    result = "Сорри, но ты ошибся нулем.";
+                }
+                else if (a == b && b == c)
+                {
+                    result = "The triangle is РАВНОСТОРОННИЙ.";
+                }
+                else if (a == b || b == c || a == c)
+                {
+                    result = "The triangle is РАВНОБЕДРЕННЫЙ.";
+                }
+                else
+                {
+                    result = "The triangle is scalene.";
+                }
+                ResultWindow resultWindow = new ResultWindow();
+                resultWindow.ResultTextBlock.Text = result;
+                resultWindow.ShowDialog();
+            }
+            catch (Exception)
+            {
+                ResultWindow resultWindow = new ResultWindow();
+                resultWindow.ResultTextBlock.Text = "Что-то не так, проверь бро";
+                resultWindow.ShowDialog();
+            }
+
+        }
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ TriYgla.io/Window1.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace TriangleApp
+{
+    public partial class ResultWindow : Window
+    {
+        public ResultWindow()
+        {
+            InitializeComponent();
+        }
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
